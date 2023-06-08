29bb5f8bbff78581cf20cd5811df368f4bb1435d
M7amd7amada <m7amd7amada@gmail.com>
2023-06-08 06:58:43 +0200

    Add Product CRUD Operations

--- BulkyWeb/Areas/Customer/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Customer/Controllers/ProductController.cs
@@ -1,6 +1,69 @@
 namespace BulkyWeb.Areas.Customer.Controllers;
 
+[Area("Customer")]
 public class ProductController : Controller
 {
+    private readonly IUnitOfWork _unitOfWork;
 
+    public ProductController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public IActionResult Index()
+    {
+        return View(_unitOfWork.ProductRepository.GetAll());
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Product product)
+    {
+        if (product is null)
+            return NotFound();
+
+        _unitOfWork.ProductRepository.Create(product);
+        _unitOfWork.Save();
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Update(int? id)
+    {
+        if (id is null || _unitOfWork.ProductRepository is null)
+            return NotFound();
+        
+
+        return View(_unitOfWork.ProductRepository.Get(p => p.ProductId == id));
+    }
+    [HttpPost]
+    public IActionResult Update(Product product)
+    {
+        if (product is null)
+            return NotFound();
+
+        _unitOfWork.ProductRepository.Update(product);
+        _unitOfWork.Save();
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        if (id is null || _unitOfWork.ProductRepository is null)
+            return NotFound();
+
+        return View(_unitOfWork.ProductRepository.Get(p => p.ProductId == id));
+    }
+    [HttpPost]
+    public IActionResult Delete(Product product)
+    {
+        if (product is null || _unitOfWork.ProductRepository is null)
+            return NotFound();
+
+        _unitOfWork.ProductRepository.Delete(product);
+        _unitOfWork.Save();
+        return RedirectToAction("Index");
+    }
 }
